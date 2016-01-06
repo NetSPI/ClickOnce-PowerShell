604f0a0e67f52a0fe4cc3261d901dc8792d4f838
egru <eric.gruber@netspi.com>
2016-01-06 17:09:54 -0600

    initial

---
+++ Visual Studio Project/ConsoleApplication2/Program.cs
@@ -0,0 +1,44 @@
+﻿//This was taken from https://github.com/Ben0xA/AwesomerShell
+
+using System;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace AwesomerShell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Powershell command
+            String cmd = "get-process;pause";
+
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+            ps.AddScript(cmd);
+
+            Collection<PSObject> output = ps.Invoke();
+            if (output != null)
+            {
+                foreach (PSObject rtnItem in output)
+                {
+                    Console.WriteLine(rtnItem.ToString());
+                }
+            }
+            rs.Close();
+            Console.Write("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
